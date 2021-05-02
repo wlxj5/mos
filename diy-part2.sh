dd87692e121143d135c2e64316367c5d0b27441e
wlxj5 <60879497+wlxj5@users.noreply.github.com>
2021-05-02 23:01:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git /package/luci-app-jd-dailybonus
+git clone https://github.com/garypang13/luci-app-eqos.git /package/luci-app-eqos
+git clone https://github.com/jerrykuku/luci-app-vssr.git /package/luci-app-vssr
+git clone https://github.com/Lienol/openwrt-OpenAppFilter.git /package/openwrt-OpenAppFilter
+git clone https://github.com/tty228/luci-app-serverchan.git /package/luci-app-serverchan
+git clone https://github.com/pymumu/luci-app-smartdns.git /package/luci-app-smartdns
