6b595f22a0bf92e1eb59fcbf33e51615c591647f
wlxj5 <60879497+wlxj5@users.noreply.github.com>
2021-05-06 13:16:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,9 +17,3 @@
 sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter' feeds.conf.default
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
