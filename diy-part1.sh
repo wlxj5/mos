692de22d60c6e7bf1db4b2b38704625c58886a05
wlxj5 <60879497+wlxj5@users.noreply.github.com>
2021-10-20 02:48:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,12 +20,9 @@ sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter' feeds.con
 sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash.git' feeds.conf.default
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/garypang13/luci-app-eqos.git package/luci-app-eqos
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/pymumu/smartdns.git feeds/packages/net/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git package/feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/AdguardTeam/AdGuardHome package/AdGuardHome
