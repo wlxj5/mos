6060baf984259620ab3998c7c23d621909aaaaa9
wlxj5 <60879497+wlxj5@users.noreply.github.com>
2021-10-21 13:26:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 sed -i '$a  src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
